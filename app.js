9e8e786dc172282a5ddae502953d3c28007d2f04
HPAnonymous <phamngochieuuit@gmail.com>
2022-05-18 23:47:50 +0700

    app.js

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('Check post request!');
+});
+
+///// SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+//
