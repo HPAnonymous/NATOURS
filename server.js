cf5c8adf2fe483b82dd65edd8a1a3b7e585d6bb5
HPAnonymous <phamngochieuuit@gmail.com>
2022-06-22 20:19:00 +0700

    Error Handling with Express

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+/* eslint no-console:  */
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exception! Shutting down...');
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
@@ -10,22 +17,30 @@ const DB = process.env.DATABASE.replace(
 );
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(DB, {
-      useNewUrlParser: true,
-      // useCreateIndex: true,
-      // useFindAndModify: false,
-    });
-    console.log('MongoDB connected!!');
-  } catch (err) {
-    console.log('Failed to connect to MongoDB', err);
-  }
+  await mongoose.connect(DB, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  });
+  console.log('MongoDB connected!!');
+  // try {
+  // } catch (err) {
+  //   console.log('Failed to connect to MongoDB', err);
+  // }
 };
 
 connectDB();
 
 ///// SERVER
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLER REJRECTION! Shutting down...');
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
 });
